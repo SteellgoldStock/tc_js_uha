578a3bd5893b357bd57756b3264db9d7b0257659
Gaëtan H <me@gaetanhus.fr>
2025-02-11 09:13:24 +0100

    Add game functionality with timer and start button; refactor cursor styles

--- scripts/game.js
+++ scripts/game.js
@@ -1,14 +1,22 @@
-import './mole.js';
-
 class Game {
   constructor() {
     this.score = 0;
     this.gameStarted = false;
-    this.elapsedTime = 0;
+    this.elapsedTime = 60;
   }
 
   start() {
+    const timerSpan = document.getElementById('timer-value');
+    timerSpan.innerHTML = this.elapsedTime;
+
     this.gameStarted = true;
+
+    setInterval(() => {
+      if (this.gameStarted) {
+        --this.elapsedTime;
+        timerSpan.innerHTML = this.elapsedTime;
+      }
+    }, 1000);
 
     const mole = new Mole();
 
@@ -23,6 +31,13 @@ class Game {
       }, 500);
     }, 2000);
   }
+
+  // parseTime() {
+  //   const minutes = Math.floor(this.elapsedTime / 60);
+  //   const seconds = this.elapsedTime % 60;
+
+  //   return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  // }
 
   stop() {
     this.gameStarted = false;
